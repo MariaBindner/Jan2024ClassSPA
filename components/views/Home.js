105750d5a5731bd3f2636ab6ce491015dd182360
Maria Bindner <maria.bindner@gmail.com>
2024-03-05 21:44:05 -0600

    mb 05Mar2024 class updates

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,8 +1,24 @@
 import html from "html-literal";
 
 export default () => html`
+  <section id="Home">
+    <header>
+      <h1>Home</h1>
+    </header>
+    <p>WIP - Home</p>
+  </section>
+
   <section id="jumbotron">
-    <h2>SavvyCoders JavaScript Fullstack Bootcamp</h2>
+    <p>MB JS Capstone</p>
     <a href="index.html">"Call to Action" "Button"</a>
   </section>
 `;
+
+// import html from "html-literal";
+
+// export default () => html`
+//   <section id="jumbotron">
+//     <h2>SavvyCoders JavaScript Fullstack Bootcamp</h2>
+//     <a href="index.html">"Call to Action" "Button"</a>
+//   </section>
+// `;
--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
-
+import axios from "axios";
+// router
 const router = new Navigo("/");
-
+// render
 function render(state = store.Home) {
   document.querySelector("#root").innerHTML = `
       ${Header(state)}
@@ -15,13 +16,53 @@ function render(state = store.Home) {
   router.updatePageLinks();
   afterRender();
 }
-
 function afterRender() {
   // add menu toggle to bars icon in nav bar
   document.querySelector(".fa-bars").addEventListener("click", () => {
     document.querySelector("nav > ul").classList.toggle("hidden--mobile");
   });
 }
+
+router.hooks({
+  before: (done, params) => {
+    // We need to know what view we are on to know what data to fetch
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
+    // Add a switch case statement to handle multiple routes
+    switch (view) {
+      // Add a case for each view that needs data from an API
+      case "Pizza":
+        // New Axios get request utilizing already made environment variable
+        axios
+          .get(`https://sc-pizza-api.onrender.com/pizzas`)
+          .then(response => {
+            // We need to store the response to the state, in the next step but in the meantime let's see what it looks like so that we know what to store from the response.
+            console.log("response", response);
+
+            store.Pizza.pizzas = response.data;
+
+            done();
+          })
+          .catch(error => {
+            console.log("It puked", error);
+            done();
+          });
+        break;
+      default:
+        done();
+    }
+  },
+  already: params => {
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
+
+    render(store[view]);
+  }
+});
 
 router
   .on({
