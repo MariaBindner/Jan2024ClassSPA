cb52eb10585c502ad8950b9b475808fa84f4cb43
Maria Bindner <maria.bindner@gmail.com>
2024-03-06 19:58:59 -0600

    Class Spa Commit 06MAR2024

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,6 +1,6 @@
 import html from "html-literal";
 
-export default () => html`
+export default state => html`
   <section id="Home">
     <header>
       <h1>Home</h1>
@@ -12,6 +12,11 @@ export default () => html`
     <p>MB JS Capstone</p>
     <a href="index.html">"Call to Action" "Button"</a>
   </section>
+  <h3>
+    The weather in ${state.weather.city} is ${state.weather.description}.
+    Temperature is ${state.weather.temp}F, and it feels like
+    ${state.weather.feelsLike}F.
+  </h3>
 `;
 
 // import html from "html-literal";
