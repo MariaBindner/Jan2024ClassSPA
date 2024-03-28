8bad1e561cf27cda7bdbe85a5ae6c2e2be10e64d
Maria Bindner <maria.bindner@gmail.com>
2024-03-27 21:44:40 -0500

    added cypress and jasmine

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    }
+  },
+  env: {
+    ROOT_URL: "http://localhost:1234",
+    PIZZA_PLACE_API_URL: "http://localhost:4040",
+    OPEN_WEATHER_MAP_API_KEY: "process.env.OPEN_WEATHER_MAP_API_KEY"
+  }
+});
---
+++ spec/pizzaRoutes.spec.js
@@ -0,0 +1,36 @@
+import request from "supertest";
+import app from "../server/app.js";
+
+describe("POST /pizzas", () => {
+  let pizzaId;
+  console.log(pizzaId);
+  it("should create a new pizza and return its _id", async () => {
+    const res = await request(app)
+      .post("/pizzas")
+      .send({
+        customer: "Tester",
+        crust: "thin",
+        sauce: "marinara",
+        cheese: "asiago",
+        toppings: ["chicken", "spinach"]
+      })
+      .expect(200); // Or any other expected HTTP status
+    expect(res.body._id).withContext("_id");
+    pizzaId = res.body._id;
+
+    const deleteResponse = await request(app)
+      .delete(`/pizzas/${pizzaId}`)
+      .set("Accept", "application/json");
+    // Assertions
+    expect(deleteResponse.status).toBe(200);
+
+    // Confirm that the pizza is deleted by sending a GET request with the same ID
+    const getResponse = await request(app)
+      .get(`/pizzas/${pizzaId}`)
+      .set("Accept", "application/json");
+    // Expect a 404 status code indicating the pizza is not found
+    expect(getResponse.status).toBe(200);
+    expect(getResponse.body).toBeNull();
+    // Add more assertions as needed
+  });
+});
